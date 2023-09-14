5505d42a17f13e8228d7d8719d0c862ee7c4997e
Marcob1996 <marcob@umd.edu>
2023-09-14 17:31:22 -0400

    Add GPU capability

--- train_test.py
+++ train_test.py
@@ -96,6 +96,7 @@ def federated_training(model, communicator, trainloader, testloader, device, los
 
         test(model, testloader, device)
 
+
 def test(model, test_dl, device, test_batches=30, epoch=False):
     correct = 0
     total = 0
@@ -104,11 +105,11 @@ def test(model, test_dl, device, test_batches=30, epoch=False):
     # since we're not training, we don't need to calculate the gradients for our outputs
     with torch.no_grad():
         for data in test_dl:
-            images, labels = data
+            inputs, labels = data
             inputs = inputs.to(device)
             labels = labels.to(device)
             # calculate outputs by running images through the network
-            outputs = model(images)
+            outputs = model(inputs)
             # the class with the highest energy is what we choose as prediction
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
