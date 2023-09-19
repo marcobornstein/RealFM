ecde57215b27d5288c9e9d48cf9584083c52418d
Marcob1996 <marcob@umd.edu>
2023-09-19 11:03:33 -0400

    8 device experiments

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ configs = {
         'test_bs': 1024,
         'lr': 1e-3,
         'a_opt': 0.9,
-        'marginal_cost': 1.073e-3,
+        'marginal_cost': 8.353e-4,
         'local_steps': 6,
         'random_seed': 1996,
         'test_frequency': 500,
-        'log_frequency': 5,
+        'log_frequency': 60,
         'test_batches': 30,
         'epochs': 100,
         'k': 18,
@@ -40,16 +40,16 @@ configs = {
 }
 
 # CIFAR 10 (Realistic): Non-Uniform (Expected Payoff is 1 (ranges between 0.9 and 1.1)) & Uniform (Payoff = 1), K=18
-# 8 Device: Expected Dataset size is 5,500 --> MC = 7.168e-4
-# train_bs = 128, local_steps = 6, mc = 7.168e-4 (8 devices)
+# 8 Device: Expected Dataset size is 5,500 --> MC = 8.353e-4
+# train_bs = 128, local_steps = 6, mc = 8.353e-4 (8 devices)
 # 16 Device: Expected Dataset size is 3,000 --> MC = 1.073e-3
 # train_bs = 128, local_steps = 6, mc = 1.073e-3 (16 devices)
 # =======================================================================
 # CIFAR 10 (Linear):
-# 8 Device: Expected Dataset size is 5,500 --> MC = 2.528e-5
-# train_bs = 128, local_steps = 6, mc = 2.528e-5 (8 devices)
-# 16 Device: Expected Dataset size is 3,000 --> MC = 5.585e-5
-# train_bs = 128, local_steps = 6, mc = 5.585e-5 (16 devices)
+# 8 Device: Expected Dataset size is 5,500 --> MC = 2.269e-5
+# train_bs = 128, local_steps = 6, mc = 2.269e-5 (8 devices)
+# 16 Device: Expected Dataset size is 3,000 --> MC = 5.403e-5
+# train_bs = 128, local_steps = 6, mc = 5.403e-5 (16 devices)
 # =======================================================================
 
 # for 16 device, payoff function between 0.9 and 1.1 because data will be zero
